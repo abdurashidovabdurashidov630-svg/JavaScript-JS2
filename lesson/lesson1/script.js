d17c2c5074302bbf65c1a2f0340731cd620110c7
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-05-18 10:04:16 +0500

    Number and Math metods

---
+++ lesson/lesson1/script.js
@@ -0,0 +1,88 @@
+// Number
+
+// 1.
+// let raqam1 = 20
+
+// 2.
+// let raqam2 = "100"
+
+// console.log(raqam1);
+// console.log(raqam2);
+
+//////////////////////////////////////
+
+// JavaScriptda raqamlar 2 ga bo'linadi.
+
+// 1. 1,2,3,4, ... 2**53 -Number
+
+// 2. 2**53 + 1, 2**53 +2, 2**53 + 3, ... - BigInt
+
+// let raqam = "27.5";
+
+// 1. parseInt()
+// raqam = parseInt(raqam)
+
+// 2. parseFloat()
+// raqam = parseFloat(raqam)
+
+// 3. Number()
+// raqam = Number(raqam)
+
+// 4. +
+// raqam = +raqam;
+
+// console.log(raqam + 3);
+
+// console.log(Number.isInteger(15));
+// console.log(Number.isInteger(15.5));
+// berilgan son kasr sonmi? yoki butun sonmi? shuni aniqlab beradi
+// va bizga doim true yoki false qaytaradi
+
+// Math methodlari.
+// let raqam = -10
+// let raqam2 = 20
+// console.log(Math.abs(raqam));
+// console.log(Math.abs(raqam2));
+
+// let raqam = 2.5432123456789098765432123;
+// console.log(Math.floor(raqam));
+// console.log(Math.ceil(raqam));
+// console.log(Math.round(raqam));
+
+// let raqam = -4.9999
+// console.log(Math.floor(raqam));
+// console.log(Math.ceil(raqam));
+// console.log(Math.round(raqam));
+
+/////////////////////////////////////////////
+
+// let raqam = 4.2511;
+// let raqam2 = 5.8164;
+// let raqam3 = 6.25123;
+
+// console.log(Math.trunc(raqam));
+// console.log(Math.trunc(raqam2));
+// console.log(Math.trunc(raqam3));
+
+// console.log(Math.trunc(Math.random() * 10));
+
+////////////////////\\\\\\\\\\\\\\\\\\\\\\
+
+// let kichik = Math.min(10,25,23,45,7,19)
+// let katta = Math.max(10,25,23,45,7,19)
+// console.log(kichik);
+// console.log(katta);
+
+// console.log(Math.pow(5,3));
+// console.log(Math.pow(2,53));
+
+// console.log(Math.sign(-5));
+// console.log(Math.sign(-10));
+// console.log(Math.sign(-7));
+// console.log(Math.sign(-13));
+// console.log(Math.sign(0));
+// console.log(Math.sign(100));
+// console.log(Math.sign(-100));
+
+// console.log(Math.sqrt(36))
+// console.log(Math.cbrt(216))
