912dfd6ce9eecf7e5aef7bf2bd0eb3e62ead82d6
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-05-20 09:41:22 +0500

    Number and Math metods

---
+++ lesson/lesson2/script.js
@@ -0,0 +1,100 @@
+// Number - 1, 2, 3, 4, ... 9007199254740992
+// console.log(2**53)
+
+// 1. 1,2,3,4, ... 9007199254740992 -Number
+
+// 2. 9007199254740992 + 1, 9007199254740992 +2, 9007199254740992 + 3, ... - BigInt
+// let num = 9007199254740992
+// console.log(typeof num);
+
+// let big = 1n
+// console.log(typeof big);
+
+// let raqam = "7.55";
+// 1. parseInt()
+// raqam = parseInt(raqam)
+
+// 2. parseFloat()
+// raqam = parseFloat(raqam)
+
+// 3. Number()
+// raqam = Number(raqam)
+
+// 4. +
+// raqam = +raqam;
+// console.log(raqam + 3);
+
+///////////////////////////////////////////////////7
+
+// isInteger()
+
+// is Integer ?
+
+// let num = 5.5;
+// console.log(Number.isInteger(num));
+
+// Math methodlari.
+
+// let a = -10
+// let b = 10
+// let s = -7
+// console.log(Math.abs(a));
+// console.log(Math.abs(b))
+// console.log(Math.abs(s))
+
+// let num1 = 3.5
+// let num2 = 3.9
+// let num3 = 3.1
+// console.log(Math.floor(num1));
+// console.log(Math.floor(num2))
+// console.log(Math.floor(num3))
+// console.log(Math.ceil(num1));
+// console.log(Math.ceil(num2))
+// console.log(Math.ceil(num3))
+// console.log(Math.round(num1));
+// console.log(Math.round(num2))
+// console.log(Math.round(num3))
+// console.log(Math.trunc(num1));
+// console.log(Math.trunc(num2))
+// console.log(Math.trunc(num3))
+
+// function nomer() {
+//     let code = Math.trunc(Math.random(90) * 99)
+//     let a = Math.trunc(Math.random()*9)
+//     let b = Math.trunc(Math.random()*9)
+//     let c = Math.trunc(Math.random()*9)
+    
+//     let d = Math.trunc(Math.random()*9)
+//     let e = Math.trunc(Math.random()*9)
+//     let f = Math.trunc(Math.random()*9)
+//     let g = Math.trunc(Math.random()*9)
+//     let tel = "+998" + " " + 93 + " " + a + b + c + " " + d + e + " " + f + g
+//     return tel
+// }
+// console.log(nomer())
+
+// console.log(Math.pow(5,3));
+// console.log(Math.pow(2,53));
+
+// let num1 = -6
+// let num2 = 5
+// let num3 = 3
+// let num4 = -8
+// let num5 = 0
+// let num6 = -0
+// console.log(Math.sign(num1));
+// console.log(Math.sign(num2));
+// console.log(Math.sign(num3));
+// console.log(Math.sign(num4));
+// console.log(Math.sign(num5));
+// console.log(Math.sign(num6));
+
+// let a = 27
+// let b = 8
+// console.log(Math.cbrt(a))
+// console.log(Math.cbrt(b))
+
+// let c = 16
+// let d = 36
+// console.log(Math.sqrt(c))
+// console.log(Math.sqrt(d))
