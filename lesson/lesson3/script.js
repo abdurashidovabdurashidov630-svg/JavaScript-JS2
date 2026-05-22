180d0661c5d98050f81663b435ae79cb783b08c1
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-05-22 09:55:22 +0500

    Methods

---
+++ lesson/lesson3/script.js
@@ -0,0 +1,71 @@
+// let nu = "2026";
+// // 1.
+// console.log(parseInt(num));
+// // 2.
+// console.log(parseFloat(num));
+// // 3.
+// console.log(Number(num));
+// // 4.
+// console.log(+num);
+
+// let num = 2026
+// // 1.
+// console.log(num.toString());
+// // 2.
+// console.log(num + "");
+// // 3.
+// console.log(String(num));
+// // 4.
+// console.log(`${num}`);
+
+// let num = 5.
+// console.log(num.toFixed(0))
+
+// let num = -4.6789
+// console.log(num.toFixed(0));
+
+// let a = "2026"
+// let b = "5b"
+
+// console.log(a / b);
+
+// NaN - Not a Number
+// isNaN - is Not a Number - berilgan qiymat Raqam emasmi?
+
+// let a = "10b"
+
+// console.log(isNan(a));
+
+// let data = "2026Algoritm2027"
+// let str = ""
+// let num = ""
+// for (let i = 0; i < data.length; i++){
+//     if (isNaN(data[i])) str += data[i]
+//     else num += data[i]
+// }
+// console.log(num,str);
+
+// let natija = 10 / 0
+// console.log(isFinite(natija))
+
+// let natija2 = 10 / 2
+// console.log(isFinite(natija2))
+
+// isFinite - qiymat cheklimi?
+// Finite - cheklangan
+
+// let a = {
+//     id:1,
+// }
+// let b = {
+//     id:1,
+// }
+// console.log(a === b);
+// console.log(Object.is(a,b));
+
+// Object.is |-| |===|
+
+// console.log(0.1 + 0.2);
+// console.log(0.6 + 0.7);
+// console.log("b" + "a" + + "a" + "a");
+// console.log(9999999999999999);
