dfc847c9af83ed32af9bf615d5a9309d8c5227b2
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-05-22 08:18:13 +0500

    Math methods

---
+++ homework/homework2/script.js
@@ -0,0 +1,92 @@
+// let son = "210.133";
+// son = parseInt(son);
+// son = parseFloat(son);
+// son = Number(son);
+// son = +son;
+// console.log(son + 241);
+
+// console.log(Math.pow(24, 3));
+// console.log(Math.pow(23, 43));
+// console.log(Math.pow(9, 13));
+// console.log(Math.pow(2421, 2));
+// console.log(Math.pow(241, 2));
+
+// console.log(Math.sign(-91));
+// console.log(Math.sign(5236));
+// console.log(Math.sign(-133));
+// console.log(Math.sign(12123));
+// console.log(Math.sign(-10));
+// console.log(Math.sign(31100));
+// console.log(Math.sign(-7100));
+
+// let raqam1 = 24.63;
+// let raqam2 = 235.3423433;
+// let raqam3 = 37.324234;
+// let raqam4 = 840.234;
+// let raqam5 = 4723.63452;
+
+// console.log(Math.trunc(raqam1));
+// console.log(Math.trunc(raqam2));
+// console.log(Math.trunc(raqam3));
+// console.log(Math.trunc(raqam4));
+// console.log(Math.trunc(raqam5));
+
+// let raqam6 = 24.422;
+// let raqam7 = 224.254;
+// let raqam8 = 242.4529;
+// let raqam9 = 29.525;
+// let raqam10 = 262.2542;
+
+// console.log(Math.round(raqam6));
+// console.log(Math.round(raqam7));
+// console.log(Math.round(raqam8));
+// console.log(Math.round(raqam9));
+// console.log(Math.round(raqam10));
+
+// console.log(Math.ceil(raqam6));
+// console.log(Math.ceil(raqam7));
+// console.log(Math.ceil(raqam8));
+// console.log(Math.ceil(raqam9));
+// console.log(Math.ceil(raqam10));
+
+// let raqam11 = 924.2343;
+// let raqam12 = 28.9992414999;
+// let raqam13 = 724.99924124999;
+// let raqam14 = 1246.9923419999;
+// let raqam15 = 124.99349999;
+
+// console.log(Math.floor(raqam11));
+// console.log(Math.floor(raqam12));
+// console.log(Math.floor(raqam13));
+// console.log(Math.floor(raqam14));
+// console.log(Math.floor(raqam15));
+
+// let raqam16 = -20;
+// let raqam17 = 60;
+// let raqam18 = -320;
+// let raqam19 = 3653;
+// let raqam20 = -34;
+
+// console.log(Math.abs(raqam16));
+// console.log(Math.abs(raqam17));
+// console.log(Math.abs(raqam18));
+// console.log(Math.abs(raqam19));
+// console.log(Math.abs(raqam20));
+
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+
+// console.log(Math.sqrt(50146));
+// console.log(Math.sqrt(5146));
+// console.log(Math.sqrt(59146));
+// console.log(Math.sqrt(59146));
+// console.log(Math.sqrt(59146));
+
+// console.log(Math.cbrt(2141));
+// console.log(Math.cbrt(345));
+// console.log(Math.cbrt(52));
+// console.log(Math.cbrt(25));
+// console.log(Math.cbrt(235));
