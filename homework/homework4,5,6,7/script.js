c3ff153be1a66698f71277c07b551cbe6610fc34
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-06-05 08:28:48 +0500

    ...

---
+++ homework/homework4,5,6,7/script.js
@@ -0,0 +1,753 @@
+// let ab1 = "Salom"
+// let ab2 = "JavaScript"
+// let ab3 = "Toshkent"
+// let ab4 = "Bugun havo issiq"
+// let ab5 = "Men dasturchiman"
+// let ab6 = "Kitob o‘qishni yaxshi ko‘raman"
+// let ab7 = "Telefon"
+// let ab8 = "Kompyuter"
+// let ab9 = "Frontend"
+// let ab10 = "Backend"
+
+// let bb1 = 'Salom'
+// let bb2 = 'JavaScript'
+// let bb3 = 'Namangan'
+// let bb4 = 'Bugun dars bor'
+// let bb5 = 'Men futbol o‘ynayman'
+// let bb6 = 'Internet tez ishlamayapti'
+// let bb7 = 'Mouse'
+// let bb8 = 'Klaviatura'
+// let bb9 = 'TV'
+// let bb10 = 'React Js'
+
+// let cb1 = `Salom`
+// let cb2 = `JavaScript`
+// let cb3 = `Andijon`
+// let cb4 = `Bugun yomg‘ir yog‘yapti`
+// let cb5 = `Men kod yozishni yaxshi ko‘raman`
+// let cb6 = `Telegram juda qulay`
+// let cb7 = `Suniy intellekt`
+// let cb8 = `Web dasturlash`
+// let cb9 = `Backend developer`
+// let cb10 = `Frontend developer`
+
+// let db1 = String(100)
+// let db2 = String(50)
+// let db3 = String(true)
+// let db4 = String(false)
+// let db5 = String(3.14)
+// let db6 = String(9999)
+// let db7 = String(null)
+// let db8 = String(undefined)
+// let db9 = String(1, 2, 3)
+// let db10 = String(2027)
+
+// let str1 = "Algoritm";
+// console.log(str1.length);
+
+// let str2 = "JavaScript";
+// console.log(str2.length);
+
+// let str3 = "Salom";
+// console.log(str3.length);
+
+// let str4 = "Frontend";
+// console.log(str4.length);
+
+// let str5 = "Backend";
+// console.log(str5.length);
+
+// let str6 = "Telefon";
+// console.log(str6.length);
+
+// let str7 = "Kompyuter";
+// console.log(str7.length);
+
+// let str8 = "Dasturlash";
+// console.log(str8.length);
+
+// let str9 = "Uzbekiston";
+// console.log(str9.length);
+
+// let str10 = "Internet";
+// console.log(str10.length);
+
+// let a1 = "Salom";
+// console.log(a1.charAt(0));
+
+// let a2 = "JavaScript";
+// console.log(a2.charAt(4));
+
+// let a3 = "Frontend";
+// console.log(a3.charAt(2));
+
+// let a4 = "Backend";
+// console.log(a4.charAt(1));
+
+// let a5 = "Telefon";
+// console.log(a5.charAt(5));
+
+// let a6 = "Kompyuter";
+// console.log(a6.charAt(3));
+
+// let a7 = "Dasturlash";
+// console.log(a7.charAt(6));
+
+// let a8 = "Uzbekiston";
+// console.log(a8.charAt(7));
+
+// let a9 = "Internet";
+// console.log(a9.charAt(2));
+
+// let a10 = "Matematika";
+// console.log(a10.charAt(4));
+
+// let b1 = "Salom";
+// console.log(b1.at(-1));
+
+// let b2 = "JavaScript";
+// console.log(b2.at(-2));
+
+// let b3 = "Frontend";
+// console.log(b3.at(-3));
+
+// let b4 = "Backend";
+// console.log(b4.at(-1));
+
+// let b5 = "Telefon";
+// console.log(b5.at(-4));
+
+// let b6 = "Kompyuter";
+// console.log(b6.at(-2));
+
+// let b7 = "Dasturlash";
+// console.log(b7.at(-5));
+
+// let b8 = "Uzbekiston";
+// console.log(b8.at(-1));
+
+// let b9 = "Internet";
+// console.log(b9.at(-3));
+
+// let b10 = "Matematika";
+// console.log(b10.at(-2));
+
+// let c1 = "A";
+// console.log(c1.charCodeAt(0));
+
+// let c2 = "B";
+// console.log(c2.charCodeAt(0));
+
+// let c3 = "C";
+// console.log(c3.charCodeAt(0));
+
+// let c4 = "D";
+// console.log(c4.charCodeAt(0));
+
+// let c5 = "E";
+// console.log(c5.charCodeAt(0));
+
+// let c6 = "F";
+// console.log(c6.charCodeAt(0));
+
+// let c7 = "G";
+// console.log(c7.charCodeAt(0));
+
+// let c8 = "H";
+// console.log(c8.charCodeAt(0));
+
+// let c9 = "I";
+// console.log(c9.charCodeAt(0));
+
+// let c10 = "J";
+// console.log(c10.charCodeAt(0));
+
+// let d1 = "Hello ";
+// let d12 = "World";
+// console.log(d1.concat(d12));
+
+// let d2 = "Java ";
+// let d22 = "Script";
+// console.log(d2.concat(d22));
+
+// let d3 = "Front ";
+// let d32 = "End";
+// console.log(d3.concat(d32));
+
+// let d4 = "Back ";
+// let d42 = "End";
+// console.log(d4.concat(d42));
+
+// let d5 = "Web ";
+// let d52 = "Site";
+// console.log(d5.concat(d52));
+
+// let d6 = "Mobile ";
+// let d62 = "App";
+// console.log(d5.concat(d62));
+
+// let d7 = "Suniy ";
+// let d72 = "Intellekt";
+// console.log(d5.concat(d72));
+
+// let d8 = "Data ";
+// let d82 = "Types";
+// console.log(d5.concat(d82));
+
+// let d9 = "Cyber ";
+// let d92 = "Security";
+// console.log(d5.concat(d92));
+
+// let d10 = "Machine ";
+// let d102 = "Learning";
+// console.log(d5.concat(d102));
+
+// let e1 = "JavaScript darsi";
+// console.log(e1.includes("darsi"));
+
+// let e2 = "Frontend kursi";
+// console.log(e2.includes("kursi"));
+
+// let e3 = "Backend dasturlash";
+// console.log(e3.includes("Backend"));
+
+// let e4 = "Suniy intellekt";
+// console.log(e4.includes("Suniy ntellekt"));
+
+// let e5 = "Web sayt";
+// console.log(e5.includes("sayt"));
+
+// let e6 = "Mobil ilova";
+// console.log(e6.includes("Mobil"));
+
+// let e7 = "Kompyuter xonasi";
+// console.log(e7.includes("telefon"));
+
+// let e8 = "Internet tez";
+// console.log(e8.includes("tez"));
+
+// let e9 = "Node.js";
+// console.log(e9.includes("Node"));
+
+// let e10 = "React Js";
+// console.log(e10.includes("Vue"));
+
+// let f1 = "Frontend";
+// console.log(f1.startsWith("Front"));
+
+// let f2 = "Backend";
+// console.log(f2.startsWith("Back"));
+
+// let f3 = "JavaScript";
+// console.log(f3.startsWith("Java"));
+
+// let f4 = "Python";
+// console.log(f4.startsWith("Py"));
+
+// let f5 = "Telefon";
+// console.log(f5.startsWith("Tel"));
+
+// let f6 = "Kompyuter";
+// console.log(f6.startsWith("Kom"));
+
+// let f7 = "Internet";
+// console.log(f7.startsWith("Inter"));
+
+// let f8 = "Dasturlash";
+// console.log(f8.startsWith("Das"));
+
+// let f9 = "Matematika";
+// console.log(f9.startsWith("Mat"));
+
+// let f10 = "Algoritm";
+// console.log(f10.startsWith("Algo"));
+
+// let g1 = "Frontend";
+// console.log(g1.endsWith("end"));
+
+// let g2 = "Backend";
+// console.log(g2.endsWith("end"));
+
+// let g3 = "JavaScript";
+// console.log(g3.endsWith("Script"));
+
+// let g4 = "Python";
+// console.log(g4.endsWith("hon"));
+
+// let g5 = "Telefon";
+// console.log(g5.endsWith("fon"));
+
+// let g6 = "Kompyuter";
+// console.log(g6.endsWith("ter"));
+
+// let g7 = "Internet";
+// console.log(g7.endsWith("net"));
+
+// let g8 = "Dasturlash";
+// console.log(g8.endsWith("lash"));
+
+// let g9 = "Matematika";
+// console.log(g9.endsWith("tika"));
+
+// let g10 = "Algoritm";
+// console.log(g10.endsWith("ritm"));
+
+// let h1 = "JavaScript darsi";
+// console.log(h1.search("darsi"));
+
+// let h2 = "Frontend kursi";
+// console.log(h2.search("kursi"));
+
+// let h3 = "Backend dasturlash";
+// console.log(h3.search("Backend"));
+
+// let h4 = "Suniy intellekt";
+// console.log(h4.search("AI"));
+
+// let h5 = "Web sayt";
+// console.log(h5.search("sayt"));
+
+// let h6 = "Mobil ilova";
+// console.log(h6.search("Mobil"));
+
+// let h7 = "Kompyuter xonasi";
+// console.log(h7.search("telefon"));
+
+// let h8 = "Internet tez";
+// console.log(h8.search("tez"));
+
+// let h9 = "Node.js";
+// console.log(h9.search("Node"));
+
+// let h10 = "React Js";
+// console.log(h10.search("a"));
+
+// let i1 = "JavaScript";
+// console.log(i1.indexOf("S"));
+
+// let i2 = "Frontend";
+// console.log(i2.indexOf("e"));
+
+// let i3 = "Backend";
+// console.log(i3.indexOf("n"));
+
+// let i4 = "Telefon";
+// console.log(i4.indexOf("f"));
+
+// let i5 = "Kompyuter";
+// console.log(i5.indexOf("p"));
+
+// let i6 = "Internet";
+// console.log(i6.indexOf("n"));
+
+// let i7 = "Dasturlash";
+// console.log(i7.indexOf("t"));
+
+// let i8 = "Matematika";
+// console.log(i8.indexOf("a"));
+
+// let i9 = "Algoritm";
+// console.log(i9.indexOf("g"));
+
+// let i10 = "Python";
+// console.log(i10.indexOf("o"));
+
+// let j1 = "banana";
+// console.log(j1.lastIndexOf("a"));
+
+// let j2 = "javascript";
+// console.log(j2.lastIndexOf("a"));
+
+// let j3 = "frontend";
+// console.log(j3.lastIndexOf("n"));
+
+// let j4 = "backend";
+// console.log(j4.lastIndexOf("e"));
+
+// let j5 = "telefon";
+// console.log(j5.lastIndexOf("o"));
+
+// let j6 = "kompyuter";
+// console.log(j6.lastIndexOf("m"));
+
+// let j7 = "internet";
+// console.log(j7.lastIndexOf("t"));
+
+// let j8 = "matematika";
+// console.log(j8.lastIndexOf("a"));
+
+// let j9 = "algoritm";
+// console.log(j9.lastIndexOf("r"));
+
+// let j10 = "python";
+// console.log(j10.lastIndexOf("h"));
+
+// let str11 = "JavaScript";
+// console.log(str11[4]);
+
+// let str12 = "Frontend";
+// console.log(str12[2]);
+
+// let str13 = "Backend";
+// console.log(str13[1]);
+
+// let str14 = "Telefon";
+// console.log(str14[5]);
+
+// let str15 = "Kompyuter";
+// console.log(str15[3]);
+
+// let str16 = "Dasturlash";
+// console.log(str16[6]);
+
+// let str17 = "Internet";
+// console.log(str17[2]);
+
+// let str18 = "Matematika";
+// console.log(str18[4]);
+
+// let str19 = "Algoritm";
+// console.log(str19[0]);
+
+// let str20 = "Python";
+// console.log(str20[3]);
+
+// let pad1 = "Algoritm";
+// console.log(pad1.padEnd(pad1.length + 3, " IT"));
+
+// let pad2 = "Kompyuter";
+// console.log(pad2.padEnd(pad2.length + 13, " Savodxonligi"));
+
+// let pad3 = "Telefon";
+// console.log(pad3.padEnd(pad3.length + 11, " Ustaxonasi"));
+
+// let pad4 = "Sotuv";
+// console.log(pad4.padEnd(pad4.length + 3, "chi"));
+
+// let pad5 = "Havdov";
+// console.log(pad5.padEnd(pad5.length + 3, "chi"));
+
+// let pad6 = "Front";
+// console.log(pad6.padEnd(pad6.length + 4, " End"));
+
+// let pad7 = "Back";
+// console.log(pad7.padEnd(pad7.length + 4, " End"));
+
+// let pad8 = "O'yinchoq";
+// console.log(pad8.padEnd(pad8.length + 7, " Zavodi"));
+
+// let pad9 = "Pul";
+// console.log(pad9.padEnd(pad9.length + 7, " Topish"));
+
+// let pad10 = "Sumka";
+// console.log(pad10.padEnd(pad10.length + 6, " Olish"));
+
+// let pad11 = "IT";
+// console.log(pad11.padStart(pad11.length + 9, "Algoritm "));
+
+// let pad12 = "Savodxonligi";
+// console.log(pad12.padStart(pad12.length + 10, "Kompyuter "));
+
+// let pad13 = "Ustaxonasi";
+// console.log(pad13.padStart(pad13.length + 8, "Telefon "));
+
+// let pad14 = "chi";
+// console.log(pad14.padStart(pad14.length + 6, "Sotuv "));
+
+// let pad15 = "chi";
+// console.log(pad15.padStart(pad15.length + 7, "Haydov "));
+
+// let pad16 = "End";
+// console.log(pad16.padStart(pad16.length + 6, "Front "));
+
+// let pad17 = "End";
+// console.log(pad17.padStart(pad17.length + 5, "Back "));
+
+// let pad18 = "Zavodi";
+// console.log(pad18.padStart(pad18.length + 10, "O'yinchoq "));
+
+// let pad19 = "Topish";
+// console.log(pad19.padStart(pad19.length + 4, "Pul "));
+
+// let pad20 = "Olish";
+// console.log(pad20.padStart(pad20.length + 6, "Sumka "));
+
+// ———————————————————————————————————————————————————————————————————————————
+// ---------------------------------------------------------------------------
+// ———————————————————————————————————————————————————————————————————————————
+
+// let repeat1 = "Lok";
+// let newRepeat1 = repeat1[0] + repeat1[1].repeat(2) + repeat1[2];
+// console.log(newRepeat1);
+
+// let repeat2 = "bok";
+// let newRepeat2 = repeat2[0] + repeat2[1].repeat(2) + repeat2[2];
+// console.log(newRepeat2);
+
+// let repeat3 = "cok";
+// let newRepeat3 = repeat3[0] + repeat3[1].repeat(2) + repeat3[2];
+// console.log(newRepeat3);
+
+// let repeat4 = "god";
+// let newRepeat4 = repeat4[0] + repeat4[1].repeat(2) + repeat4[2];
+// console.log(newRepeat4);
+
+// let repeat5 = "mod";
+// let newRepeat5 = repeat5[0] + repeat5[1].repeat(2) + repeat5[2];
+// console.log(newRepeat5);
+
+// let repeat6 = "hok";
+// let newRepeat6 = repeat6[0] + repeat6[1].repeat(2) + repeat6[2];
+// console.log(newRepeat6);
+
+// let repeat7 = "hod";
+// let newRepeat7 = repeat7[0] + repeat7[1].repeat(2) + repeat7[2];
+// console.log(newRepeat7);
+
+// let repeat8 = "hop";
+// let newRepeat8 = repeat8[0] + repeat8[1].repeat(2) + repeat8[2];
+// console.log(newRepeat8);
+
+// let repeat9 = "hot";
+// let newRepeat9 = repeat9[0] + repeat9[1].repeat(2) + repeat9[2];
+// console.log(newRepeat9);
+
+// let repeat10 = "por";
+// let newRepeat10 = repeat10[0] + repeat10[1].repeat(2) + repeat10[2];
+// console.log(newRepeat10);
+
+// let replace1 = "Algoritm yoki Oxford"
+// replace1 = replace1.replace("O", "A")
+// replace1 = replace1.replaceAll("O", "A")
+// console.log(replace1);
+
+// let replace2 = "Oxford yoki Algoritm"
+// replace2 = replace2.replace("yoki", "bilan")
+// replace2 = replace2.replaceAll("yoki", "bilan")
+// console.log(replace2);
+
+// let replace3 = "Telefon va Televizor"
+// replace3 = replace1.replace("e", "o")
+// replace3 = replace1.replaceAll("e", "o")
+// console.log(replace3);
+
+// let replace4 = "Qanday?"
+// replace4 = replace1.replace("a", "o")
+// replace4 = replace1.replaceAll("a", "o")
+// console.log(replace4);
+
+// let replace5 = "Lampochka va Svet farqi:"
+// replace5 = replace1.replace("A", "O")
+// replace5 = replace1.replaceAll("A", "O")
+// console.log(replace5);
+
+// let replace6 = "Acer yoki Macbook"
+// replace6 = replace1.replace("A", "e")
+// replace6 = replace1.replaceAll("A", "e")
+// console.log(replace6);
+
+// let replace7 = "Eshik yoki Deraza"
+// replace7 = replace1.replace("e", "a")
+// replace7 = replace1.replaceAll("e", "a")
+// console.log(replace7);
+
+// let replace8 = "Deraza yoki Eshik"
+// replace8 = replace1.replace("e", "a")
+// replace8 = replace1.replaceAll("e", "a")
+// console.log(replace8);
+
+// let replace9 = "Jo'ja, o'ylamoq"
+// replace9 = replace1.replace("o'", "u")
+// replace9 = replace1.replaceAll("o'", "u")
+// console.log(replace9);
+
+// let replace10 = "Dasturlash - programming"
+// replace10 = replace10.replace("a", "o'")
+// replace10 = replace10.replaceAll("a", "o'")
+// console.log(replace10);
+
+// let orginal1 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy1 = orginal1.slice(10,12)
+// let copy1 = orginal1.substring(10,12)
+// console.log(copy1);
+
+// let orginal2 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy2 = orginal2.slice(35)
+// let copy2 = orginal2.substring(35)
+// console.log(copy2);
+
+// let orginal3 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy3 = orginal3.slice(0,4)
+// let copy3 = orginal3.substring(0,4)
+// console.log(copy3);
+
+// let orginal4 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy4 = orginal4.slice(4,9)
+// let copy4 = orginal4.substring(4,9)
+// console.log(copy4);
+
+// let orginal5 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy5 = orginal5.slice(13,21)
+// let copy5 = orginal5.substring(13,21)
+// console.log(copy5);
+
+// let orginal6 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy6 = orginal6.slice(21,27)
+// let copy6 = orginal6.substring(21,27)
+// console.log(copy6);
+
+// let orginal7 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy7 = orginal7.slice(27,30)
+// let copy7 = orginal7.substring(27,30)
+// console.log(copy7);
+
+// let orginal8 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy8 = orginal8.slice(30,32)
+// let copy8 = orginal8.substring(30,32)
+// console.log(copy8);
+
+// let orginal9 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy9 = orginal9.slice(32,36)
+// let copy9 = orginal9.substring(32,36)
+// console.log(copy9);
+
+// let orginal10 = "Men bugun JS darsini o'rganmoqdaman."
+// let copy10 = orginal10.slice(0,2)
+// let copy10 = orginal10.substring(0,2)
+// console.log(copy10);
+
+// ———————————————————————————————————————————————————————————————————————————
+// ---------------------------------------------------------------------------
+// ———————————————————————————————————————————————————————————————————————————
+
+// let str = "Algoritm, Oxford, It, English,"
+// let arr = str.split(" ")
+// console.log(arr);
+
+// let str2 = "Algoritm, Oxford, It, English,"
+// let arr2 = str2.split(",")
+// console.log(arr2);
+
+// let str3 = "Algoritm, Oxford, It, English,"
+// let arr3 = str3.split("g")
+// console.log(arr3);
+
+// let str4 = "Algoritm, Oxford, It, English,"
+// let arr4 = str4.split("o")
+// console.log(arr4);
+
+// let str5 = "Algoritm, Oxford, It, English,"
+// let arr5 = str5.split()
+// console.log(arr5);
+
+// let trim3 = "Kompyuter";
+// let trim1 = "                  Sotib                  ";
+// trim1 = trim1.trim()
+// trim1 = trim1.trimStart();
+// trim1 = trim1.trimEnd();
+// let trim2 = "Olish";
+// console.log(trim3 + trim1 + trim2);
+
+// let trim6 = "Sumka";
+// let trim4 = "                  Sotib                  ";
+// trim4 = trim4.trim()
+// trim4 = trim4.trimStart();
+// trim4 = trim4.trimEnd();
+// let trim5 = "Olish";
+// console.log(trim6 + trim4 + trim5);
+
+// let trim9 = "It";
+// let trim7 = "                  Sohasini                  ";
+// trim7 = trim7.trim()
+// trim7 = trim7.trimStart();
+// trim7 = trim7.trimEnd();
+// let trim8 = "O'rganish";
+// console.log(trim9 + trim7 + trim8);
+
+// let trim12 = "Ingiliz";
+// let trim10 = "                  Tilini                  ";
+// trim10 = trim10.trim()
+// trim10 = trim10.trimStart();
+// trim10 = trim10.trimEnd();
+// let trim11 = "O'rganish";
+// console.log(trim12 + trim10 + trim11);
+
+// let trim15 = "Rus";
+// let trim13 = "                  Tilini                  ";
+// trim13 = trim13.trim()
+// trim13 = trim13.trimStart();
+// trim13 = trim13.trimEnd();
+// let trim14 = "O'rganish";
+// console.log(trim15 + trim13 + trim14);
+
+// let up1 = "algoritm";
+// let low1 = "ALGORITM";
+// console.log(up1.toUpperCase());
+// console.log(low1.toLowerCase());
+
+// let up2 = "oxford";
+// let low2 = "OXFORD";
+// console.log(up2.toUpperCase());
+// console.log(low2.toLowerCase());
+
+// let up3 = "kompyuter";
+// let low3 = "KOMPYUTER";
+// console.log(up3.toUpperCase());
+// console.log(low3.toLowerCase());
+
+// let up4 = "telefon";
+// let low4 = "TELEFON";
+// console.log(up4.toUpperCase());
+// console.log(low4.toLowerCase());
+
+// let up5 = "planshet";
+// let low5 = "PLANSHET";
+// console.log(up5.toUpperCase());
+// console.log(low5.toLowerCase());
+
+// let a = "Dunyo"
+// let b = "yo"
+// console.log(a.localeCompare(b));
+
+// let c = "Kompyuter"
+// let d = "py"
+// console.log(c.localeCompare(d));
+
+// let e = "Salom"
+// let f = "Salom"
+// console.log(e.localeCompare(f));
+
+// let g = "dunyo"
+// let h = "olam"
+// console.log(g.localeCompare(h));
+
+// let i = "Telefon"
+// let j = "eleon"
+// console.log(i.localeCompare(j));
+
+// let sum = "23- 81 * -2 / 5.9"
+// console.log(eval(sum));
+
+// let sum2 = "76394-2424+919*2392218/2"
+// console.log(eval(sum2));
+
+// let sum3 = "526-2828+2/2*2"
+// console.log(eval(sum3));
+
+// let sum4 = "526+828+2/2*4"
+// console.log(eval(sum4));
+
+// let sum5 = "58+2-5/5*854/2"
+// console.log(eval(sum5));
+
+// console.log(String.fromCharCode(65));
+// console.log(String.fromCharCode(97));
+// console.log(String.fromCharCode(125));
+// console.log(String.fromCharCode(45));
+// console.log(String.fromCharCode(951));
+
+// console.log(String.fromCodePoint(0x1F612));
+// console.log(String.fromCodePoint(0x1F133));
+// console.log(String.fromCodePoint(0x1F576));
+// console.log(String.fromCodePoint(0x1F699));
+// console.log(String.fromCodePoint(0x1F611));
